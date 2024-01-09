6be20b29042e604b7d963e01c12a4687251d244f
emerson espinoza <xinayespinoza@gmail.com>
2024-01-09 14:31:44 -0300

    feat stock references con product

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :products
+    
+    resources :products do
+      resources :stocks
+    end
     resources :categories
   end
   devise_for :admins
