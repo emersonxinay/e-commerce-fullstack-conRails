59d065b6bb92009f79366d6f607fd83b9270c0c1
emerson <xinayespinoza@gmail.com>
2024-01-21 19:38:26 -0300

    fix render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,5 @@
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+./bin/rails assets:precompile
+./bin/rails assets:clean
